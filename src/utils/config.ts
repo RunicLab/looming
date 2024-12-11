9fa71affabd6bc4f9cd0a1cca02abe23ae41bead
sijirama <gbemilesanmi@gmail.com>
2024-12-11 21:56:30 +0100

    fix: deployment issue

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
 
 export const CONFIG = {
-    URL: process.env.APP_URL || "http://localhost:3000",
+    //URL: process.env.APP_URL || "http://localhost:3000",
+    URL: process.env.APP_URL || "https://looming.siji.ng",
 }
